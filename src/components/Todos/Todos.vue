cb8afde33b6193300d32f068e6c554faf296d0a3
tiboCorb <corbrionthibault@gmail.com>
2018-06-03 22:13:11 +0200

    add filter and deleting methodes

--- src/components/Todos/Todos.vue
+++ src/components/Todos/Todos.vue
@@ -5,18 +5,27 @@
             <input type="text" class="new-todo" placeholder="Ajouter une tache" v-model='newTodo' @keyup.enter="addTodo">
         </header>
         <div class="main">
+          <input type="checkbox" class='toggle-all' v-model="allDone">
             <ul class="todo-list">
-                <li v-for="todo in todos" :key="todo.name" class="todo " :class="{ completed :todo.completed}">
+                <li v-for="todo in filtered" :key="todo.name" class="todo " :class="{ completed :todo.completed, editing : todo === editing}">
                     <div class="view">
                         <input class="toggle" type="checkbox" v-model="todo.completed">
-                        <label>{{todo.name}}</label>
+                        <label @dblclick="editTodo(todo)">{{todo.name}}</label>
+                        <button class="destroy" @click.prevent="deleteTodo(todo)"></button>
                     </div>
+                      <input type="text" class="edit" v-model="todo.name" @keyup.enter="doneEdit" v-focus="todo === editing">
                 </li>
             </ul>
         </div>
         <div>
-            <footer class="footer">
-                <span class="todo-count"> Tâche à faire <strong>{{remaning}}</strong></span>
+            <footer class="footer" v-show="todos.length > 0">
+                <span class="todo-count"><strong>{{remaning}}</strong> tâche à faire </span>
+                <ul class="filters">
+                  <li><a href="%23" :class="{selected: filter === 'all'}" @click.prevent="filter = 'all'">Toutes</a></li>
+                  <li><a href="%23" :class="{selected: filter === 'todo'}" @click.prevent="filter ='todo'">A faire</a></li>
+                  <li><a href="%23" :class="{selected: filter === 'done'}" @click.prevent="filter = 'done'">Faites</a></li>
+                </ul>
+                <button class="clear-completed" v-show="hasDone" @click.prevent="deleteDone">Clean</button>
             </footer>
         </div>
     </section>
@@ -30,10 +39,24 @@ export default {
         name: 'Tache test',
         completed: false
       }],
-      newTodo: ''
+      newTodo: '',
+      filter: 'all',
+      editing: null
     }
   },
   methods: {
+    doneEdit () {
+      this.editing = null
+    },
+    deleteTodo (obj) {
+      this.todos = this.todos.filter(todo => todo !== obj)
+    },
+    deleteDone () {
+      this.todos = this.todos.filter(todo => !todo.completed)
+    },
+    editTodo (todo) {
+      this.editing = todo
+    },
     addTodo () {
       this.todos.push({
         completed: false,
@@ -43,8 +66,39 @@ export default {
     }
   },
   computed: {
+    hasDone () {
+      return this.todos.filter(todo => todo.completed).length > 0
+    },
+    allDone: {
+      get () {
+        return this.remaning === 0
+      },
+      set (value) {
+        this.todos.forEach(todo => {
+          todo.completed = value
+        })
+      }
+    },
     remaning () {
       return this.todos.filter(todo => !todo.completed).length
+    },
+    filtered () {
+      switch (this.filter) {
+        case 'all':
+          return this.todos
+        case 'todo':
+          return this.todos.filter(todo => !todo.completed)
+        case 'done':
+          return this.todos.filter(todo => todo.completed)
+      }
+    }
+  },
+  directives: {
+    focus (el, value) {
+      console.log(value)
+      if (value) {
+        el.focus()
+      }
     }
   }
 }
